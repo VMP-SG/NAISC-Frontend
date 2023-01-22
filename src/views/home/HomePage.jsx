2fa224eebc49aec64d9ccdac494706d9972fc3fe
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-01-23 00:24:40 +0800

    Added Data Streaming

--- src/views/home/HomePage.jsx
+++ src/views/home/HomePage.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from '../../components/Sidebar'
 
 const HomePage = ({ open, onOpenSidebar, onCloseSidebar }) => {
+  const [data, setData] = useState("");
+  useEffect(() => {
+    const sse = new EventSource(import.meta.env.VITE_BACKEND_URL + "/test");
+    sse.onmessage = (e) => {
+      setData(e.data);
+    }
+
+    sse.onerror = () => {
+      console.log("Error");
+      sse.close();
+    }
+
+    return () => sse.close();
+  },[])
   return (
     <>
       <Sidebar open={open} onOpen={onOpenSidebar} onClose={onCloseSidebar} />
       <p>This is the home page.</p>
+      <p>Data: {data}</p>
     </>
   )
 }
