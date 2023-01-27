3395891662d5c88263fefc5b8e31a7c9c5293618
noahxinjie <noahxinjie@gmail.com>
2023-01-27 23:57:46 +0800

    Add Dropdown component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,12 @@
+import Dropdown from "./Dropdown"
+
 const Card = ({text}) => {
     return (
         <div className="bg-white shadow-lg w-3/4 h-4/5 p-6 mt-20 ml-60 rounded">
-            <p className="font-bold text-2xl">{text}</p>
+            <div className="flex flex-row justify-between">
+                <p className="font-bold text-2xl">{text}</p>
+                <Dropdown/>
+            </div>
         </div>
     )
 }
